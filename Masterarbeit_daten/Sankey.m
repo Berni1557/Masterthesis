ab14eded1a89ec69aa8f890343c2b9c3c0c2491b
Berni <SearchPart@yahoo.com>
2015-02-15 20:33:59 +0100

    Add sankey, RANSAC,...

--- Masterarbeit_daten/Sankey.m
+++
@@ -1,8 +0,0 @@
-% Sankey
-
-inputs = [100]; losses = [10 5 60 20]; unit = '%'; sep = [1,2,3];
-labels = {'Bauteile','Bauteil nicht erkannt','Package nicht erkannt','Bauteilbezeichnung nicht erkannt','Price to low','Reuse'};
-drawSankey(inputs, losses, unit, labels, sep);
-
-
-
